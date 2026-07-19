0d101759b312e6b0e35524fda96cd4d3bde80474
gaurav maurya <gauravmaurya9756@gmail.com>
2026-07-19 22:46:46 +0530

    fix: force dark mode on discord hub to match app theme

--- client/src/app/discord/page.tsx
+++ client/src/app/discord/page.tsx
@@ -8,9 +8,10 @@ const DiscordHub = dynamic(() => import("@/components/discord/DiscordHub"), { ss
 export default function DiscordPage() {
   return (
     <AppShell>
-      <div className="flex flex-col h-full min-h-[calc(100vh-80px)] w-full">
+      <div className="dark flex flex-col h-full min-h-[calc(100vh-80px)] w-full">
         <DiscordHub />
       </div>
     </AppShell>
   );
 }
+
